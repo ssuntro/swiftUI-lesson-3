3f8708a25042e9cea6793d97eafc0a20fae1f6e4
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 20:03:33 +0700

    finish layout ui with NavigationView and list

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }.navigationTitle("Hacker News")
+        }
+        
     }
 }
 
@@ -19,3 +23,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "One"),
+    Post(id: "2", title: "Two"),
+    Post(id: "3", title: "three"),
+]
