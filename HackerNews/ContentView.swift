c4ddab0eecb1bba7112faaee5b62ec6d4acbcdd8
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 20:53:13 +0700

    add NetworkManager to be a ObservableObject and UI be a listener on published prop is updated

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager() //in order to listen published data, subscribe to the networkmanager when ever networkManager is updated this line going to trigger and ui will update accordingly
     var body: some View {
         NavigationView {
-            List(posts) { post in
+            List(networkManager.posts) { post in
+                //when ever networkManager.posts is updated swiftUI will trigger to rebuild this list
+                Text(String(post.points))
                 Text(post.title)
             }.navigationTitle("Hacker News")
-        }
-        
+        }.onAppear(perform: { //view load -> appear
+            self.networkManager.fetchData()
+        })
     }
 }
 
@@ -24,13 +28,13 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "One"),
-    Post(id: "2", title: "Two"),
-    Post(id: "3", title: "three"),
-]
+//struct Post: Identifiable {
+//    let id: String
+//    let title: String
+//}
+//
+//let posts = [
+//    Post(id: "1", title: "One"),
+//    Post(id: "2", title: "Two"),
+//    Post(id: "3", title: "three"),
+//]
--- HackerNews/NetworkManager.swift
+++ HackerNews/NetworkManager.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-class NetworkManager {
+class NetworkManager: ObservableObject { // Make NetworkManager observable(observer design pattern)
+    
+    @Published var posts = [Post]() //published prop for any interested parties and notify all changes to the listener
     func fetchData() {
         if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
             let session = URLSession(configuration: .default)
@@ -15,7 +17,10 @@ class NetworkManager {
                 if error == nil,
                    let data = data,
                    let result = try? JSONDecoder().decode(Result.self, from: data) {
-                    ///observer design pattern
+                    DispatchQueue.main.async {
+                        self.posts = result.hits //Must be upedate on the main thread due to published prop
+                    }
+                    
                 }
             }
             task.resume()
