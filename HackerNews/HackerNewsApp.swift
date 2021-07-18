412fcda594ae839732abc34f07eb3da2b090edce
Siroratt Suntronsuk <soulyu_shine@hotmail.com>
2021-07-18 19:48:42 +0700

    initial project

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Siroratt Suntronsuk on 18/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
